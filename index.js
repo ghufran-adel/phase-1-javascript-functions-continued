5d0077da67cf5aec1f4927873fb73a367391f97e
ghufran adel <gho0ofran.adel14@gmail.com>
2022-05-03 12:27:55 -0700

    compelet

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun (string="roller-skate"){
+ return`This Saturday, I want to ${string}!`;
+    
+}
+saturdayFun (string="bathe my dog");
+saturdayFun ();
+
+
+
+function mondayWork(string ="go to the office"){
+    return `This Monday, I will ${string}.`;
+
+}
+function wrapAdjective (Fname="*"){
+    
+    return  function(Plang ="special"){
+      return `You are ${Fname}${Plang}${Fname}!`
+    };
+    
+    }
